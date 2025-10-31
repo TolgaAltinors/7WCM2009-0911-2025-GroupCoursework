974db62bc20cfe40ed1f51c905394ef27b0f5d27
benjmoh <bentothejie@gmail.com>
2025-10-31 00:55:24 +0000

    Implemented main function with validation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,20 +8,88 @@ fun main() {
 
         when (input) {
             "1" -> {
-                print("Enter integer to calculate the sum: ")
+                while(true) {
+                    print("Enter integer to calculate the sum: ")
+                    val number = readln().toIntOrNull()
+                    if(number != null && number > 0){
+                        println(sum(number))
+                        break
+                    }else{
+                        println("Invalid input. Please enter a number greater than zero.")
+                    }
+                }
 
             }
 
             "2" -> {
-                print("Select unit to convert from: ")
+                val validUnits = listOf("C", "F", "K")
+                tempConvertLoop@ while(true){
+                    print("Enter a temperature value: ")
+                    val number = readln().toDoubleOrNull()
+                    if(number != null){
+                        while(true){
+                            print("Select unit to convert from (C,F,K): ")
+                            val unitFrom = readln().trim().uppercase()
+                            if(unitFrom in validUnits){
+                                if(unitFrom == "K" && number < 0) {
+                                    println("Kelvin can not be negative.")
+                                    continue
+                                }
+                                    while (true) {
+                                        print("Select unit to convert to: ")
+                                        val unitTo = readln().trim().uppercase()
+                                        if (unitTo in validUnits) {
+                                            if (unitTo != unitFrom) {
+                                                println(convertTemperature(number, unitFrom, unitTo))
+                                                break@tempConvertLoop
+                                            } else {
+                                                println("Units must be different.")
+                                            }
+                                        } else {
+                                            println("Invalid unit. Please enter C, F or K.")
+                                        }
+                                    }
 
+                            }else{
+                                println("Invalid unit. Please enter C, F or K.")
+                            }
+                        }
+                    }else{
+                        println("Invalid number. Please enter a numeric value.")
+                    }
+                }
 
-                print("Select unit to convert to: ")
 
             }
 
             "3" -> {
-                print("Enter integer for factorial calculation: ")
+               factorialLoop@ while(true) {
+                    print("Enter 1st integer for factorial calculation: ")
+                    val number = readln().trim().toIntOrNull()
+                    if (number != null && number >= 0) {
+                        while(true) {
+                            print("2nd number: ")
+                            val number2 = readln().trim().toIntOrNull()
+                            if(number2 != null && number2 >= 0){
+                                while(true) {
+                                    print("3rd number: ")
+                                    val number3 = readln().trim().toIntOrNull()
+                                    if (number3 != null && number3 >= 0) {
+                                        println(factorial(number, number2, number3))
+                                        break@factorialLoop
+                                    } else {
+                                        println("Invalid 3rd number input. Please enter a non-negative integer.")
+                                    }
+                                }
+                            }else{
+                                println("Invalid 2nd number input. Please enter a non-negative integer.")
+                            }
+                        }
+                    } else {
+                        println("Invalid 1st number input. Please enter a non-negative integer.")
+
+                    }
+                }
 
                 // **************************************
                 // VALIDATION TO BE ADDED
