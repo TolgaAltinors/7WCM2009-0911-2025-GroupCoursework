5b9bde23165c346ac6a9a94764d4e24c033b4f65
TolgaAltinors <50206787+TolgaAltinors@users.noreply.github.com>
2025-11-02 09:57:16 +0000

    Amend factorial.kt to cater for 3 user inputs.

--- src/main/kotlin/factorial.kt
+++ src/main/kotlin/factorial.kt
@@ -1,35 +1,49 @@
 import java.math.BigInteger
 /**
  * Compute the factorial of user input
- * @param n greater than or equal to 0.
+ * @param n1, n2, n3 greater than or equal to 0.
  * @return the factorial of n as part of a string.
  */
-fun factorial(n: Int): String {
+fun factorial(n1: Int, n2: Int, n3: Int): String {
 
-    // variable to hold the calculation unless there is a overflow error
-    var factorial_result: Long = 1L
-
-    if (n <= 0) {
-        return "Number provided failed validation: $n not greater than Zero"
+    /**
+     * Calculate the factorial of user input
+     * @param n
+     * @return the factorial of n
+     */
+    fun calculateFactorial(n: Int): BigInteger {
+        var tempResult = BigInteger.ONE
+        for (i in 2..n) {
+            tempResult = tempResult.multiply(BigInteger.valueOf(i.toLong()))
+        }
+        return tempResult
     }
+
+    // Create list of numbers passed to function
+    val numbers = listOf(n1, n2, n3)
+
+    // variable to hold the calculation
+    var factorialTotal = BigInteger.ZERO
+
     try {
         // Loop for creating the sum
-        // Math.multiplyExact provides an overflow exception if Long.MAX_VALUE is exceeded
-        for (i in 2..n) {
-            factorial_result =
-                Math.multiplyExact(factorial_result, i.toLong())  // to detect the overflow and raises error
+
+        for (j in numbers) {
+
+            val result = calculateFactorial(j)
+
+            println("Factorial of $j is: $result")
+
+            factorialTotal += result
+            println("Factorial running total is: $factorialTotal")
         }
-        return "Factorial of $n is: $factorial_result"
-    } catch (e: ArithmeticException) {
-        // We encountered an overflow exception. We will use BigInteger instead
-        var factorial_result_big = BigInteger.ONE
-        // Loop for creating the sum
-        for (i in 2..n) {
-            factorial_result_big = factorial_result_big.multiply(BigInteger.valueOf(i.toLong()))
-        }
-        return "Factorial of $n is: $factorial_result_big"
-    } catch (e: Exception) {
+
+        return "Factorial of $n1 + $n2 + $n3 is: $factorialTotal"
+
+    }
+    catch (e: Exception) {
         // default catch-all
         return "Unexpected error: ${e.message}"
     }
 }
+
